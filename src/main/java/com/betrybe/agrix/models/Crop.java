e5d4633fdcc80d270c2efa2ef8c05bcb002c436f
Nathan <nathanlucaass.silva@gmail.com>
2024-01-17 15:18:14 -0300

    database completed

---
+++ src/main/java/com/betrybe/agrix/models/Crop.java
@@ -0,0 +1,122 @@
+package com.betrybe.agrix.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+/**
+ * The type Crop.
+ */
+@Entity
+@Table(name = "crop")
+public class Crop {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  @Column(name = "planted_area")
+  private Double plantedArea;
+
+  @ManyToOne
+  @JoinColumn(name = "farm_id")
+  private Farm farm;
+
+  /**
+   * Instantiates a new Crop.
+   */
+  public Crop() {
+  }
+
+  /**
+   * Instantiates a new Crop.
+   *
+   * @param name        the name
+   * @param plantedArea the planted area
+   * @param farm        the farm
+   */
+  public Crop(String name, Double plantedArea, Farm farm) {
+    this.name = name;
+    this.plantedArea = plantedArea;
+    this.farm = farm;
+  }
+
+  /**
+   * Gets id.
+   *
+   * @return the id
+   */
+  public Long getId() {
+    return id;
+  }
+
+  /**
+   * Sets id.
+   *
+   * @param id the id
+   */
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  /**
+   * Gets name.
+   *
+   * @return the name
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Sets name.
+   *
+   * @param name the name
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   * Gets planted area.
+   *
+   * @return the planted area
+   */
+  public Double getPlantedArea() {
+    return plantedArea;
+  }
+
+  /**
+   * Sets planted area.
+   *
+   * @param plantedArea the planted area
+   */
+  public void setPlantedArea(Double plantedArea) {
+    this.plantedArea = plantedArea;
+  }
+
+  /**
+   * Gets farm.
+   *
+   * @return the farm
+   */
+  public Farm getFarm() {
+    return farm;
+  }
+
+  /**
+   * Sets farm.
+   *
+   * @param farm the farm
+   */
+  public void setFarm(Farm farm) {
+    this.farm = farm;
+  }
+}
